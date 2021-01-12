26dbcb20478584fa5c52050a317d37185dc7c899
tadsturmab <tadsturmab2014@gmail.com>
2021-01-12 14:48:36 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -33,7 +33,7 @@ public class Order implements Serializable {
 	@JoinTable(name = "tb_order_product",
 	joinColumns = @JoinColumn(name = "order_id"),
 	inverseJoinColumns = @JoinColumn(name = "product_id"))
-	private Set<Product> Products = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 	
 	public Order() {
 		
@@ -98,7 +98,7 @@ public class Order implements Serializable {
 	}
 
 	public Set<Product> getProducts() {
-		return Products;
+		return products;
 	}
 
 	@Override
