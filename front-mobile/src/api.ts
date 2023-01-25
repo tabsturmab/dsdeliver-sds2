64f6b5d55958eae744329a2f6bb0456eab3be74f
Andre Silva <Andre Silva>
2023-01-25 11:25:28 -0300

    Correções

--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+//http://192.168.0.26:8085
 //https://andre-aulas-sds2.herokuapp.com
-const API_URL = 'http://192.168.0.26:8085';
+const API_URL = 'https://andre-aulas-sds2.herokuapp.com';
 
 export function fetchOrders() {
     return axios(`${API_URL}/orders`);
